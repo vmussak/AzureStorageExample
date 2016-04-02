bf710dc0f11f854d3fb9c16784bca428a3992ea3
Vinicius Mussak <vinicius.mussak@outlook.com>
2016-04-01 23:32:44 -0300

    Commit

---
+++ src/AzureStorageExample/Controllers/UploadController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorageExample.Controllers
+{
+    public class UploadController : Controller
+    {
+        /*
+        https://mussak.core.windows.net/documentos/cliente1.pdf
+        */
+        private readonly CloudStorageAccount _storageAccount;
+
+        public UploadController(IConfigurationRoot configurationRoot)
+        {
+            //Recupera a conta de armazenamento pela connection string
+            _storageAccount = CloudStorageAccount.Parse(configurationRoot["StorageConnectionString"]);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upload(IFormFile arquivo)
+        {
+            //Cria um blob client
+            CloudBlobClient blobClient = _storageAccount.CreateCloudBlobClient();
+
+            //Recupera a referencia do container documentos
+            CloudBlobContainer container = blobClient.GetContainerReference("documentos");
+
+            //Caso não exista, ele cria
+            container.CreateIfNotExists();
+
+            //Setar permissão de acesso para 'público'
+            container.SetPermissions(
+                new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob }
+            );
+
+            //Recupera a referência de um blob chamado 'cliente01'
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("cliente01.pdf");
+
+            Stream streamFile = arquivo.OpenReadStream();
+
+            //Cria ou substitui o blob com o conteúdo do upload
+            blockBlob.UploadFromStream(streamFile);
+
+            return RedirectToAction("VerArquivos");
+        }
+
+        public IActionResult VerArquivos()
+        {
+
+            //Cria um blob client
+            CloudBlobClient blobClient = _storageAccount.CreateCloudBlobClient();
+
+            //Recupera a referencia do container documentos
+            CloudBlobContainer container = blobClient.GetContainerReference("documentos");
+
+            //Caso não exista, ele cria
+            container.CreateIfNotExists();
+
+            //Setar permissão de acesso para 'público'
+            container.SetPermissions(
+                new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob }
+            );
+
+            //Recupera os arquivos de um container
+            var arquivos = container.ListBlobs();
+
+            return View("Documentos", arquivos);
+        }
+
+        public IActionResult Excluir()
+        {
+            //Cria um blob client
+            CloudBlobClient blobClient = _storageAccount.CreateCloudBlobClient();
+
+            //Recupera a referencia do container documentos
+            CloudBlobContainer container = blobClient.GetContainerReference("documentos");
+
+            //Caso não exista, ele cria
+            container.CreateIfNotExists();
+
+            //Setar permissão de acesso para 'público'
+            container.SetPermissions(
+                new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob }
+            );
+
+            //Recupera a referência de um blob chamado 'cliente01'
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("cliente01.pdf");
+
+            //Cria ou substitui o blob com o conteúdo do upload
+            blockBlob.DeleteIfExists();
+
+            return RedirectToAction("VerArquivos");
+        }
+    }
+}
---
+++ src/AzureStorageExample/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AzureStorageExample
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(x => Configuration);
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Information);
+            loggerFactory.AddDebug();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
